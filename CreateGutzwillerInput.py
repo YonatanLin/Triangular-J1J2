3a8cdd16b61ade15e91ff4c10d827a30dfcb7ed0
YonatanLin <76913435+YonatanLin@users.noreply.github.com>
2026-04-19 14:31:11 +0300

    small changes

--- CreateGutzwillerInput.py
+++ CreateGutzwillerInput.py
@@ -13,11 +13,11 @@ def CreateGutzwillerCaseDirFromInputFile(main_results_dir, input_file):
         input_file_lines = file.readlines()
     params = input_file_lines[0].split(" ")
     print(params)
-    assert(params[0] == "Lx" and params[1] == "Ly" and params[2] == "chi")
+    assert(params[0] == "Lx" and params[1] == "Ly" and params[2] == "chi\n")
     input_for_condor = open("gutz_condor_cases.txt", 'w')
     for line in input_file_lines[1:]:
         params = line.split(" ")
-        case_folder = CreateGutzwillerCaseDir(main_results_dir, params[0], params[1], params[2])
+        case_folder = CreateGutzwillerCaseDir(main_results_dir, params[0], params[1], params[2].split("\n")[0])
         input_for_condor.write(line[:-1] + " " + case_folder + "\n")
 
 if __name__ == "__main__":

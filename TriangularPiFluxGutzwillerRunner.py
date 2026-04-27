5587935d1d1549264ea61f202d621d9aacea276c
YonatanLin <76913435+YonatanLin@users.noreply.github.com>
2026-04-27 15:41:17 +0300

    added flux to Gutzwiller cluster runner

--- TriangularPiFluxGutzwillerRunner.py
+++ TriangularPiFluxGutzwillerRunner.py
@@ -1,6 +1,9 @@
 import argparse
 
+from numpy import float128
+
 from Trying2D import TriangularPiFluxGutzwiller
+
 
 def build_parser():
     parser = argparse.ArgumentParser(
@@ -9,12 +12,13 @@ def build_parser():
     parser.add_argument("--Lx", type=int, required=True, help="Number of unit cells along x.")
     parser.add_argument("--Ly", type=int, required=True, help="Number of unit cells along y.")
     parser.add_argument("--chi_max", type=int, required=True, help="Max bond dimension.")
-
+    parser.add_argument("--flux", type=float, required=True, help="Net flux threaded through the cylinder")
     return parser
+
 
 def main():
     args = build_parser().parse_args()
-    TriangularPiFluxGutzwiller(Lx=args.Lx, Ly=args.Ly, chi_max=args.chi_max)
+    TriangularPiFluxGutzwiller(Lx=args.Lx, Ly=args.Ly, chi_max=args.chi_max, flux=args.flux)
 
 
 if __name__ == "__main__":
--- TryingTemfpy.py
+++ TryingTemfpy.py
@@ -1,4 +1,4 @@
-local = False
+local = True
 
 from temfpy import slater
 import temfpy.gutzwiller as gutz

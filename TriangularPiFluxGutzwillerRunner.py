fc2c126bdce644273ba5c5476d833fb05d4dfb08
YonatanLin <76913435+YonatanLin@users.noreply.github.com>
2026-05-02 09:58:03 +0300

    added geometry option to pi-flux Gutzwiller cluster runs

--- TriangularPiFluxGutzwillerRunner.py
+++ TriangularPiFluxGutzwillerRunner.py
@@ -13,12 +13,14 @@ def build_parser():
     parser.add_argument("--Ly", type=int, required=True, help="Number of unit cells along y.")
     parser.add_argument("--chi_max", type=int, required=True, help="Max bond dimension.")
     parser.add_argument("--flux", type=float, required=True, help="Net flux threaded through the cylinder")
+    parser.add_argument("--geometry", type=str, required=True, help="YC or XC geometry")
     return parser
 
 
 def main():
     args = build_parser().parse_args()
-    TriangularPiFluxGutzwiller(Lx=args.Lx, Ly=args.Ly, chi_max=args.chi_max, flux=args.flux)
+    TriangularPiFluxGutzwiller(Ly=args.Ly, geometry=args.geometry, Lx=args.Lx, chi_max=args.chi_max,
+                               flux=args.flux)
 
 
 if __name__ == "__main__":

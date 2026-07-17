49e8c3e3d1e4709a4def355b08de887082fbf69e
YonatanLin <76913435+YonatanLin@users.noreply.github.com>
2026-07-17 14:59:16 +0300

    see prev commit

---
+++ CreateOverlapsInput.py
@@ -0,0 +1,10 @@
+import sys
+from Trying2D import CreateOverlapsCaseDir
+from ClusterInputConfigurations import CreateTriangularCaseDirFromInputFile, dmrg_gutz_comp_input_params
+
+
+if __name__ == "__main__":
+    assert(sys.argv[1][-1] == "/")
+    CreateTriangularCaseDirFromInputFile(sys.argv[1], sys.argv[2], dmrg_gutz_comp_input_params,
+                                         [], CreateOverlapsCaseDir,
+                                         "gutz_condor_cases.txt")
